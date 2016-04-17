8f9cb4611ac473541d336434060629e1f89ac72d
Samuel Martins <samuelmartins.sw@gmail.com>
2016-04-16 23:40:16 -0300

    Some minimal adjustments

--- app/index.js
+++ app/index.js
@@ -9,11 +9,22 @@ const error = {
 }
 
 const api = {
+  /**
+   *
+   * @get /index
+   *
+   */
+
   index (req, res){
     const msg = `Ta tranquilo, ta favoravel!`
     res.json({ msg })
   },
 
+  /**
+   *
+   * @get /users
+   *
+   */
   users (req, res){
     const users = user.getUsers()
 
@@ -23,16 +34,60 @@ const api = {
         results: users
       })
     }).catch(err => error.internalServerError(err, req, res))
+  },
+
+  /**
+   *
+   * @get /users/:id
+   *
+   */
+  user (req, res){
+    const singleUser = user.getUser(req.params.id)
+
+    singleUser.then(user => {
+      console.log(user)
+      res.json({
+        result: user
+      })
+    }).catch(err => error.internalServerError(err, req, res))
+  },
+
+  /**
+   *
+   * @post /users
+   *
+   */
+  createUser (req, res){
+    const userData = {
+      name: req.query.name
+    }
+
+    const createUser = user.createUser(userData)
+
+    createUser.then(user => {
+      res.json({
+        msg: `User ${userData.name} created!`
+      })
+    }).catch(err => error.internalServerError(err, req, res))
   }
 }
 
 const PORT = process.env.PORT || 3000
 const app = express()
+
 /**
+ *
  * Routes
+ *
  */
-app.get(`/`, api.index)
-app.get(`/users`, api.users)
+
+ /** GET **/
+app.get('/', api.index)
+app.get('/users', api.users)
+app.get('/users/:id', api.user)
+
+/** POST **/
+app.post('/users', api.createUser)
 
 app.listen(PORT, function(){
 	console.log(`Listening on port ${PORT}`)
--- app/user.js
+++ app/user.js
@@ -1,7 +1,15 @@
 import connection from './connection'
 
 export default {
-  getUsers () {
+  getUsers (){
     return connection.query('SELECT * FROM users')
+  },
+
+  getUser (id){
+    return connection.query('SELECT * FROM users U WHERE U.id = ' + id)
+  },
+
+  createUser (data){
+    return connection.query(`INSERT INTO users (name) VALUES ('${data.name}')`)
   }
 }
